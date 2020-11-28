fcb462b59b19dcb2f4e23eb698d42828932a0dc4
Oles <omelanchuk98@gmail.com>
2020-11-29 00:00:08 +0200

    Finished main page

--- Teacher/Views/Main/Controls/CardButton.xaml.cs
+++ Teacher/Views/Main/Controls/CardButton.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -47,6 +48,9 @@ namespace Teacher.Views.Main.Controls
             set => SetValue(TextPositionProperty, value);
         }
 
+        public VerticalAlignment TextPositionCast 
+            => (VerticalAlignment) TextPosition;
+
         public static readonly DependencyProperty TextAlignmentProperty = DependencyProperty.Register(
             "TextAlignment", typeof(TextAlignment), typeof(CardButton), new PropertyMetadata(default(TextAlignment)));
 
@@ -56,9 +60,76 @@ namespace Teacher.Views.Main.Controls
             set => SetValue(TextAlignmentProperty, value);
         }
 
+        public static readonly DependencyProperty TextBackgroundProperty = DependencyProperty.Register(
+            "TextBackground", typeof(Color), typeof(CardButton), new PropertyMetadata(default(Color)));
+
+        public Color TextBackground
+        {
+            get => (Color) GetValue(TextBackgroundProperty);
+            set => SetValue(TextBackgroundProperty, value);
+        }
+
         public CardButton()
         {
             this.InitializeComponent();
+            RegisterPropertyChangedCallback(TextAlignmentProperty, CaptionChangedCallback);
+            RegisterPropertyChangedCallback(TextProperty, CaptionChangedCallback);
+            RegisterPropertyChangedCallback(TextBackgroundProperty, CaptionChangedCallback);
+        }
+
+        private void CaptionChangedCallback(DependencyObject sender, DependencyProperty dp)
+        {
+            var gradientFill = 0.27;
+            var textAlign = TextAlignment;
+
+            var fillBrush = new LinearGradientBrush();
+            if (textAlign == TextAlignment.Center)
+            {
+                fillBrush.StartPoint = new Point(0, 0.5);
+                fillBrush.EndPoint = new Point(1, 0.5);
+                var transparentZone = 1 - gradientFill;
+                fillBrush.GradientStops.Add(
+                    new GradientStop
+                    {
+                        Color = Colors.Transparent,
+                        Offset = transparentZone / 2
+                    });
+                fillBrush.GradientStops.Add(new GradientStop
+                {
+                    Color = TextBackground,
+                    Offset = 0.5
+                });
+                fillBrush.GradientStops.Add(new GradientStop
+                {
+                    Color = Colors.Transparent,
+                    Offset = 1 - transparentZone / 2
+                });
+            }
+            else
+            {
+                if (textAlign == TextAlignment.Right)
+                {
+                    fillBrush.StartPoint = new Point(1 - gradientFill, 0.5);
+                    fillBrush.EndPoint = new Point(0, 0.5);
+                }
+                else
+                {
+                    fillBrush.StartPoint = new Point(gradientFill, 0.5);
+                    fillBrush.EndPoint = new Point(1, 0.5);
+                }
+
+                fillBrush.GradientStops.Add(new GradientStop
+                {
+                    Color = TextBackground,
+                    Offset = 0
+                });
+                fillBrush.GradientStops.Add(new GradientStop
+                {
+                    Offset = 1
+                });
+            }
+
+            CaptionBackground.Background = fillBrush;
         }
     }
 }
