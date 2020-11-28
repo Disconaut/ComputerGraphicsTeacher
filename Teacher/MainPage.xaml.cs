fa04b598290d84ccd0b662f0fc8f619c2863da96
Oles <omelanchuk98@gmail.com>
2020-11-28 18:28:47 +0200

    Implemented basic navigations

--- Teacher/MainPage.xaml.cs
+++ Teacher/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Teacher.Views.Errors;
+using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
+using NavigationViewBackRequestedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs;
+using NavigationViewSelectionChangedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +27,54 @@ namespace Teacher
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly Type _underConstructionPageType;
+
         public MainPage()
         {
             this.InitializeComponent();
+            _underConstructionPageType = typeof(UnderConstruction);
+
+            MainNavigation.SelectedItem =
+                MainNavigation.MenuItems
+                    .OfType<Microsoft.UI.Xaml.Controls.NavigationViewItem>()
+                    .First();
+
+            MainNavigation.UpdateLayout();
+        }
+
+        private void MainNavigation_OnSelectionChanged(NavigationView sender,
+            NavigationViewSelectionChangedEventArgs args)
+        {
+            const string viewsNamespace = "Teacher.Views";
+
+            var selectedTab = args.SelectedItem as NavigationViewItem;
+
+            var pageTypeFullName = selectedTab?.Tag as string;
+            var pageType = Type.GetType($"{viewsNamespace}.{pageTypeFullName}");
+            if (pageType?.IsSubclassOf(typeof(Page)) ?? false)
+            {
+                MainFrame.Navigate(pageType, pageTypeFullName);
+            }
+            else
+            {
+                MainFrame.Navigate(_underConstructionPageType, pageTypeFullName);
+            }
+        }
+
+        private void MainNavigation_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                var backStackLength = MainFrame.BackStack.Count;
+                var backPage = MainFrame.BackStack.Last();
+                MainFrame.BackStack.RemoveAt(backStackLength - 1);
+
+                var backPageTypeName = backPage.Parameter as string;
+
+                MainNavigation.SelectedItem = MainNavigation.MenuItems
+                    .OfType<Microsoft.UI.Xaml.Controls.NavigationViewItem>()
+                    .First(x => x.Tag as string == backPageTypeName);
+            }
         }
     }
 }
